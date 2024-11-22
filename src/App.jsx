6d590b1cd10bf9ae21cba48e7e684d863e78e9ba
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-11-22 23:00:58 +0600

    final update

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import Nav from "./components/Nav/Nav"
-import Navbar from "./components/Navbar/Navbar"
-
-
+import './App.css'
+import LineChart from './components/LineChart/LineChart'
+import NavBar from './components/NavBar/NavBar'
+import Phones from './components/Phones/Phones'
+import PriceOptions from './components/PriceOptions/PriceOptions'
+// import DaisyNav from './components/DaisyNav/DaisyNav'
 
 function App() {
 
   return (
-    <div className="box-content w-11/12 mx-auto">
-      {/* <Navbar></Navbar> */}
-      <Nav></Nav>
-    </div>
+    <>
+      <NavBar></NavBar>
+      {/* <DaisyNav></DaisyNav> */}
+      <h1 className='text-7xl bg-rose-600'>Vite + React</h1>
+      <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
+      <Phones></Phones>
+    </>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,60 +1,37 @@
+import { useState } from "react";
+import Link from "../Link/Link";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+
+const NavBar = () => {
+    const [open, setOpen] = useState(false)
+    const routes = [
+        { id: 1, path: '/', name: 'Home' },
+        { id: 2, path: '/about', name: 'About' },
+        { id: 3, path: '/services', name: 'Services' },
+        { id: 4, path: '/contact', name: 'Contact' },
+        { id: 5, path: '*', name: 'NotFound' }
+    ];
 
 
-const Navbar =()=>{
-    return(
-        <div className="navbar bg-green-300">
-            <div className="navbar-start">
-                <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor">
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h8m-8 6h16" />
-                    </svg>
-                </div>
-                <ul
-                    tabIndex={0}
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                    <li><a>Item 1</a></li>
-                    <li>
-                    <a>Parent</a>
-                    <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                    </ul>
-                    </li>
-                    <li><a>Item 3</a></li>
-                </ul>
-                </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+    return (
+        <nav className="text-black p-6 bg-yellow-200">
+            <div className=" md:hidden text-2xl" onClick={() => setOpen(!open)}>
+                {
+                    open === true ? 
+                    <AiOutlineClose></AiOutlineClose> 
+                    : <AiOutlineMenu className=""></AiOutlineMenu>
+                }
+                
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                <li><a>Item 1</a></li>
-                <li>
-                    <details>
-                    <summary>Parent</summary>
-                    <ul className="p-2">
-                        <li><a>Submenu 1</a></li>
-                        <li><a>Submenu 2</a></li>
-                    </ul>
-                    </details>
-                </li>
-                <li><a>Item 3</a></li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Button</a>
-            </div>
-        </div>
+            <ul className={`md:flex duration-1000 absolute md:static
+            ${open ? 'top-16': '-top-60'}
+            bg-yellow-200 px-6`}>
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+        </nav>
     );
 };
 
-export default Navbar;
+export default NavBar;
