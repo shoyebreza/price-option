819fb3542934ec5899ee20f6f928fffeac047a7f
shoyeb <68115608+shoyebreza@users.noreply.github.com>
2024-11-22 12:17:00 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
+import Navbar from "./components/Navbar/Navbar"
 
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-green-500">
-      Hello world!
-      </h1>
-    </>
+    <div className="box-content w-11/12 mx-auto">
+      <Navbar></Navbar>
+    </div>
   )
 }
 
